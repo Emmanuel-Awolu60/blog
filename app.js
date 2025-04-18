88517678321744e9cbd00c9cb7a229759297d18c
dev_eao <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2025-04-18 19:44:13 +0100

    Added some new file

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 // Template engnine
 app.use(expressLayout);
-app.set('layout', './layout/maine');
+app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'));
